7c39c0a4e7d0200b8926091f26e8048a06aaa63f
깃wooqqq <sge0225@naver.com>
2023-03-20 16:45:04 +0900

    SQL SELECT

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,6 +1,15 @@
 package com.mysite.sbb;
 
+import org.hibernate.boot.model.relational.QualifiedSequenceName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
+
+    Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -5,27 +5,92 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.awt.desktop.QuitStrategy;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
-	@Autowired
-	private QuestionRepository questionRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
 
-	@Test
-	@DisplayName("데이터 저장")
-	void t001() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q1);  // 첫번째 질문 저장
+    @Test
+    @DisplayName("데이터 저장")
+    void t001() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);  // 첫번째 질문 저장
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q2);  // 두번째 질문 저장
-	}
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);  // 두번째 질문 저장
+    }
 
+    /*
+    SQL
+    SELECT * FROM question
+     */
+    @Test
+    @DisplayName("findAll")
+    void t002() {
+        List<Question> all = questionRepository.findAll();
+        assertEquals(2, all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
+    /*
+    SQL
+    SELECT *
+    FROM question
+    WHERE id = 1
+     */
+    @Test
+    @DisplayName("findByID")
+    void t003() {
+        Optional<Question> oq = questionRepository.findById(1);
+
+        if (oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
+
+    /*
+    SQL
+    SELECT *
+    FROM question
+    WHERE subject = 'sbb가 무엇인가요?'
+     */
+    @Test
+    @DisplayName("findBySubject")
+    void t004() {
+        Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectAndContent")
+    void t005() {
+        Question q = questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다."
+        );
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectAndContent")
+    void t006() {
+        List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
 }

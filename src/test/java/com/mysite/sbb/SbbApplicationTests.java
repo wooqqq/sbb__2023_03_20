62cecb63a4548e507f90e72629d17775284be7b0
깃wooqqq <sge0225@naver.com>
2023-03-21 15:12:57 +0900

    에러 수정

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -209,7 +209,7 @@ class SbbApplicationTests {
 
         List<Answer> answerList = q.getAnswerList();
 
-        assertEquals(1. answerList.size());
+        assertEquals(1, answerList.size());
         assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }

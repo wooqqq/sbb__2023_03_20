ad60f9e88dc48f2d18ff661f0dd19d609d1c9f40
깃wooqqq <sge0225@naver.com>
2023-03-21 11:07:24 +0900

    각 테스트케이스의 독립성 확보

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.mysite.sbb;
 
 import org.hibernate.boot.model.relational.QualifiedSequenceName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -12,4 +13,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
+
+    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,5 +1,7 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,20 +22,39 @@ class SbbApplicationTests {
     // 얘는 데이터 저장이니까 한번만 들어가야함
     // 두번 들어가면 오류날 수도 있음
     // 그럴땐 sql에서 init.sql의 테이블 내용 초기화를 실행하자
-    @Test
-    @DisplayName("데이터 저장")
-    void t001() {
+
+    @BeforeEach
+    void beforeEach() {
+        // 모든 데이터 삭제
+        questionRepository.deleteAll();
+
+        // 흔적 삭제 (다음번 INSERT 때 id가 1번으로 설정되도록)
+        questionRepository.clearAutoIncrement();
+
+        // 질문 1개 생성
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
         q1.setContent("sbb에 대해서 알고 싶습니다.");
         q1.setCreateDate(LocalDateTime.now());
         questionRepository.save(q1);  // 첫번째 질문 저장
 
+        // 질문 1개 생성
         Question q2 = new Question();
         q2.setSubject("스프링부트 모델 질문입니다.");
         q2.setContent("id는 자동으로 생성되나요?");
         q2.setCreateDate(LocalDateTime.now());
         questionRepository.save(q2);  // 두번째 질문 저장
+    }
+    @Test
+    @DisplayName("데이터 저장")
+    void t001() {
+        Question q = new Question();
+        q.setSubject("세계에서 가장 부유한 국가가 어디인가요?");
+        q.setContent("알고 싶습니다.");
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
+
+        assertEquals("세계에서 가장 부유한 국가가 어디인가요?", questionRepository.findById(3).get().getSubject());
     }
 
     /*

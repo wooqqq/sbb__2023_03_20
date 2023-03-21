2aba4f3abb55bf8e98f2a97a2a79101c8daab49a
깃wooqqq <sge0225@naver.com>
2023-03-21 12:52:23 +0900

    답변 데이터 생성 후 저장하기

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -14,6 +14,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     List<Question> findBySubjectLike(String subject);
 
+    // @Modifying // 만약 아래 쿼리가 UPDATE, DELETE, INSERT 라면 이걸 붙여야 한다.
+    // nativeQuery = true 여야 MySQL 쿼리문법 사용 가능
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
 }

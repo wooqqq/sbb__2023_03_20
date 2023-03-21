f723e2b717b45e57521604e59148e1fa30362a2c
깃wooqqq <sge0225@naver.com>
2023-03-21 17:01:49 +0900

    타임리프 적용

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,12 @@
+package com.mysite.sbb.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class QuestionController {
+    @GetMapping("/question/list")
+    public String list() {
+        return "question_list";
+    }
+}

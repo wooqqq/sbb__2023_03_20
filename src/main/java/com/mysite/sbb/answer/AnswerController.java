08dcaa0eb42d8461a7e08a43f9ff352190798cd5
깃wooqqq <sge0225@naver.com>
2023-03-22 15:20:56 +0900

    답변 저장하기

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -5,22 +5,27 @@ import com.mysite.sbb.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+
+@Controller
 @RequestMapping("/answer")
 @RequiredArgsConstructor
-@Controller
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
-        Question question = this.questionService.getQuestion(id);
+    @ResponseBody
+    public String createAnswer(Model model, @PathVariable Integer id, @RequestParam String content) {
+        Question question = questionService.getQuestion(id);
 
         // TODO : 답변객체를 만든다.
+        Answer answer = answerService.create(question, content);
+
+        // 아래와 같이 처리요청에 대해서는, 처리 후 빨리 떠나는 게 좋다.
+        // 단 적절한 if 문을 사용하면 유용한 방법이 될 수 도 있다. 즉 머무는게 좋은 경우도 있음
+        // return "%d번 질문에 대한 답변이 생성되었습니다.(답변번호 : %d)".formatted(id, answer.getId());
 
         return "redirect:/question/detail/%d".formatted(id);
     }
---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public Answer create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        answerRepository.save(answer);
+
+        return answer;
+    }
+}

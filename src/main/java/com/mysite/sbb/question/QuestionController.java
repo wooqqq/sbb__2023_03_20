d89e3fe901e612dd138b0904a6102a6c1b793815
깃wooqqq <sge0225@naver.com>
2023-03-22 12:51:41 +0900

    질문 상세 컨트롤러 만들기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.question;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,5 +21,12 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
 
         return "question_list";
+    }
+
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id")Integer id) {
+        model.addAttribute("id", id);
+
+        return "question_detail";
     }
 }

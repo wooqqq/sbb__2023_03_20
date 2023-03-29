3a36aeaff98f311fc17af1565f4eb745b8058e42
깃wooqqq <sge0225@naver.com>
2023-03-29 13:37:23 +0900

    마크다운 파서 적용

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+// 컴포넌트 이름 : commonUtil
+// 이 컴포넌트를 타임리프에서 접근할때 #commonUtil
+public class CommonUtil {
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
